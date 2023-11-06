73e1b9790fafd0770d4e7575c0244182abb3ff05
Jawad Ali <jawad@eddress.co>
2023-11-06 23:15:47 +0100

    Added support for WatchOS with Push notifications view

---
+++ WatchLandmarks Watch App/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Watch App
+//
+//  Created by jawad ali on 06/11/2023.
+//
+
+import SwiftUI
+import UserNotifications
+
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .task {
+                let center = UNUserNotificationCenter.current()
+                _ = try? await center.requestAuthorization(
+                    options: [.alert, .sound, .badge]
+                )
+            }
+    }
+}
+
+
+#Preview {
+    ContentView()
+        .environment(ModelData())
+}
